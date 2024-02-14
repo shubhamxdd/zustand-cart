8cb9cb6ad2a043399a03a4613140de078dab13ac
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2024-02-14 12:32:16 +0530

    Sticky navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      <div className="bg-stone-600 text-white px-8 p-4  flex justify-between items-center">
+      <div className="bg-stone-600 text-white px-8 p-4  flex justify-between items-center sticky top-0">
         <Link to={"/"}>
           <h1 className="text-xl font-semibold">StoreName</h1>
         </Link>
